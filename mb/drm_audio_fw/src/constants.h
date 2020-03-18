4a297c67c8ee5481af97e354016dc9fd55743bf3
tancy <mintancy@hotmail.com>
2020-03-18 11:24:00 -0400

    Update constants.h

--- mb/drm_audio_fw/src/constants.h
+++ mb/drm_audio_fw/src/constants.h
@@ -7,7 +7,7 @@
 //from constants.h mitre file:
 #define MAX_SONG_SZ (1<<25) //33554432 == 32 mib (more than system ram lol)
 
-#define PKEY_SIZE 32 //see: eddsa keygen
+#define PKEY_SIZE 64 //see: hmac keygen
 #define UNAME_SIZE 16 //see: ectf requirements (it is actually 15, but each name is nul-padded to 16 for obvious reasons)
 #define SALT_SIZE 16 //see: common sense
 #define PIN_SIZE 64 //see: ectf requirement
